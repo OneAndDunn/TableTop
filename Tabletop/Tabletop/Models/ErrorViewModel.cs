29aebd82c29f7b8aab5c005000ba811cf9aa03f8
Unknown <immaturestephen@gmail.com>
2018-03-29 17:06:55 +1100

    ASP.net core initialising project

---
+++ Tabletop/Tabletop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tabletop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
